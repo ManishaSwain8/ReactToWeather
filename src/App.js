7d9a24a91adf7b978645323ebba4502e8a3c3f5c
Manisha Swain <manishaswain2003@gmail.com>
2023-10-06 22:28:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { MantineProvider, createTheme } from "@mantine/core";
+import "./App.css";
+import axios from "axios";
+import "@mantine/core/styles.css";
+import Home from "./Components/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <MantineProvider>
+        <Home />
+      </MantineProvider>{" "}
     </div>
   );
 }

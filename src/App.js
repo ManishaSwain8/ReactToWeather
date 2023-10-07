beae647d5722fae1e4591af88d7a8c8d7cc8399a
unknown <shiveshtiwari0@gmail.com>
2023-10-07 12:13:15 +0530

    Optimized the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { MantineProvider, createTheme } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
-import axios from "axios";
 import "@mantine/core/styles.css";
 import Home from "./Components/Home";
 function App() {

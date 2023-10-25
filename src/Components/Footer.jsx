7779cc80fef8eb96f85d27951b09d1f897e7d6d7
abhay-raj19 <rajpootabhay423@gmail.com>
2023-10-25 21:25:31 +0530

    fix:footer_issues

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
-export default function Footer() {
+import React from "react";
+import logo from "../assets/favicon.png";
+function Footer() {
   return (
-    <footer className="bg-purple-800 text-white p-4 text-center">
-      <p>Crafted with ❤️ for the Thriving Open-Source Community</p>
+    <footer className="bg-purple-800 p-1 text-white text-center">
+      <div className="flex justify-center items-center">
+        <img
+          src={logo} // Replace with actual image path
+          alt="Logo"
+          className="mr-1 w-6 h-6"
+        />
+        <span className="font-bold">ReactToWeather</span>
+      </div>
+      <div className="mt-2">
+        <a href="/about" className="mx-2">
+          About Us
+        </a>
+        <a href="/contact" className="mx-2">
+          Contact Us
+        </a>
+        <a href="/program" className="mx-2">
+          Our Program
+        </a>
+        <a href="/support" className="mx-2">
+          Support
+        </a>
+        <a href="/plans" className="mx-2">
+          Plans
+        </a>
+      </div>
     </footer>
-  )
+  );
 }
+
+export default Footer;
+
+
+
+
